513bb55c934ebae4a8c5c59bacf3a4f598fd3cd3
Hecate-vv <wdobrodziej@gmail.com>
2024-05-04 18:41:52 +0200

    zad2

---
+++ company/implementations/EmailMessenger.java
@@ -0,0 +1,8 @@
+package company.implementations;
+import company.interfaces.Messenger;
+public class EmailMessenger implements Messenger {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email message: " + message);
+    }
+}
