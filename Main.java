753290bd3f83613697ca71fff78f9164db087d5f
Hecate-vv <wdobrodziej@gmail.com>
2024-05-04 19:00:07 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 import company.implementations.EmailMessenger;
-
+import company.utils.MathUtils;
 public class Main {
     public static void main(String[] args) {
+        int result = MathUtils.add(5, 3);
+        System.out.println("Result of adding: " + result);
+
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Hello, this is an email message.");
+        emailMessenger.sendMessage("The result is: " + result);
     }
 }
